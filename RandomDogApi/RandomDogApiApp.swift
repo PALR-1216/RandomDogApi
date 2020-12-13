dc40789c8431e48319d80fe04372a2dd759c8424
PALR-1216 <palr12162000@gmail.com>
2020-12-13 19:12:55 -0400

    Initial Commit

---
+++ RandomDogApi/RandomDogApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDogApiApp.swift
+//  RandomDogApi
+//
+//  Created by Pedro Alejandro on 12/13/20.
+//
+
+import SwiftUI
+
+@main
+struct RandomDogApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
